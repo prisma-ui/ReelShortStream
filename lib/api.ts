f5c16cda5da3d4138f697c26d8a95dfb8c20699c
prisma-ui <srrexus@gmail.com>
2026-05-19 13:10:45 +0700

    Add cache option to apiFetch function

--- lib/api.ts
+++ lib/api.ts
@@ -70,6 +70,7 @@ export interface EpisodeDetail {
 async function apiFetch<T>(path: string): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
     headers: { 'Content-Type': 'application/json' },
+    cache: 'no-store',
   });
   if (!res.ok) throw new Error(`API error: ${res.status}`);
   return res.json();
